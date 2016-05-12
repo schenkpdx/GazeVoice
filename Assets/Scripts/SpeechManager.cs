0a25f07b4ed143eac74a05475fbaaa75e3105272
Chris Schenk <schenkpdx@gmail.com>
2016-05-11 23:46:04 -0700

    first commit

---
+++ Assets/Scripts/SpeechManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : MonoBehaviour
+{
+
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+    void Start ()
+    {
+	    keywords.Add("Make Color Blue", () =>
+	    {
+	        var focusObject = GazeGestureManager.Instance.FocusedObject;
+	        if (focusObject != null)
+	        {
+	            focusObject.SendMessage("MakeColorBlue");
+	        }
+	    });
+
+        keywords.Add("Make Color Red", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                focusObject.SendMessage("MakeColorRed");
+            }
+        });
+
+        keywords.Add("Reset", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                focusObject.SendMessage("Reset");
+            }
+        });
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/SphereCommands.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereCommands : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+    void MakeColorBlue()
+    {
+        this.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    void MakeColorRed()
+    {
+        this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void Reset()
+    {
+        this.gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+    }
+}
